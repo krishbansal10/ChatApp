d6e26c788184420c9eec9e90de7c9132eeb7f433
Krish <krishbansal2005@gmail.com>
2024-03-27 15:24:03 +0530

    final

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+        config.externals.push({
+            "utf-8-validate": "commonjs utf-8-validate",
+            bufferutil: "commonjs bufferutil"
+        });
+
+        return config;
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "utfs.io**",
+                port: "",
+            },
+        ]
+    }
+};
+
+export default nextConfig;
